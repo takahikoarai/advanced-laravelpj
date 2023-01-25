8eca7ed99242b08f9e365e9e9a29829f7c15a413
takahikoarai <araitakahiko@gmail.com>
2023-01-26 00:22:16 +0900

    lesson 'test'-3

--- tests/Feature/HelloTest.php
+++ tests/Feature/HelloTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class HelloTest extends TestCase
 {
@@ -13,15 +14,21 @@ class HelloTest extends TestCase
      *
      * @return void
      */
+
+    use RefreshDatabase;
+
     public function testHello()
     {
-        $this->assertTrue(true);
+        User::factory()->create([
+            'name'=>'aaa',
+            'email'=>'bbb@ccc.com',
+            'password'=>'test12345'
+        ]);
 
-        $response = $this->get('/');
-        $response->assertStatus(200);
-
-        $response = $this->get('/no_route');
-        $response->assertStatus(404);
-
+        $this->assertDatabaseHas('users',[
+            'name'=>'aaa',
+            'email'=>'bbb@ccc.com',
+            'password'=>'test12345'
+        ]);
     }
 }
